9977825bee81bd7d5c5dc621b8aa8b03044810bb
Cloudmaster124 <Simondineshansen@gmail.com>
2024-11-15 12:43:34 +0100

    ikke fungere

--- 11_OOP_Intro/Clock.js
+++ 11_OOP_Intro/Clock.js
@@ -1,7 +1,12 @@
+
+//når en klasses objekter kan opføre sig forskeligt iforhold til construktoren,
+//klades det POLYMORFI
 class Clock{
     //constructor er klassen "setup" funktion som kaldes når nye objekter med klasssen
-    constructor(div){
+    constructor(div, style){
         this.div = div
+        this.shape = style.shape
+        this.background = style.background
         //div's for hours, minutes, secunds
         this.hDiv = createDiv()
         this.mDiv = createDiv()
@@ -11,6 +16,36 @@ class Clock{
         this.div.child(this.sDiv)
         //interval til at sætte tiden ind
         this.interval
+        //styles
+        this.div.style('width', '16rem')
+        this.div.style('height', '5rem')
+        this.div.style('border', '4px dotted gray')
+        this.div.style('display', 'grid')
+        this.div.style('place-items', 'center')
+        this.div.style('padding', '1rem')
+        this.div.style('border-radius', '2rem')
+        //reager på argumenntet style fra constructoren
+        switch(this.shape){
+            case 'cirkel':
+                this.div.style('height', '16rem')
+                this.div.style('border-radius', '50%')
+                break
+            case 'ellipse':
+                this.div.style('height', '8rem')
+                this.div.style('border-radius', '50%')
+                break
+
+        }
+
+        switch(this.background){
+            case 'pink': 
+                this.div.style('background','hotpink')
+                break
+            case 'black': 
+                this.div.style('background','black')
+                this.div.style('color', 'white')
+                break
+        }
 
     }
     start(){
@@ -21,5 +56,8 @@ class Clock{
             this.mDiv.html(minute() < 10 ?'0' + minute() : minute() )
             this.sDiv.html(second() < 10 ?'0' + second() : second() )
         },1000)
+    }
+    stop(){
+        clearInterval(this.interval)
     }
 }
--- 11_OOP_Intro/index.js
+++ 11_OOP_Intro/index.js
@@ -1,7 +1,10 @@
 function setup(){
     //Variablen c, er en ny instans af klassen Clock, som får div'en #clock med i sin construktor
-    let c = new Clock( select('#clock'))
+    //json objekt med styling parametre sendes som det andet argument
+    let style = {
+        background: 'black',
+        shape: 'cirkel'
+    }
+    let c = new Clock( select('#clock') , style)
     c.start()
 }
-
-//ttest
