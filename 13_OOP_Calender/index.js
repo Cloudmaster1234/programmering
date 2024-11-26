6761784dab79c8baed6951c4a71f6f7c31631108
Cloudmaster124 <Simondineshansen@gmail.com>
2024-11-26 09:05:19 +0100

    virker ikke

---
+++ 13_OOP_Calender/index.js
@@ -0,0 +1,11 @@
+let classContainer
+
+function preload() {
+}
+
+function setup() {
+  //HTML containeren
+  calendarContainer = select('#calender')
+
+  let door = new door(calendarContainer, "12", '·/assets/jul.jpg')
+}
